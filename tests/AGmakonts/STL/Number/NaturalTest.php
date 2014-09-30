b21e9dc001467a2c90f81f08c23a82605a44ea06
AGmakonts <agmakonts@gmail.com>
2014-09-30 23:47:44 +0200

    more tests

---
+++ tests/AGmakonts/STL/Number/NaturalTest.php
@@ -0,0 +1,66 @@
+<?php
+use AGmakonts\STL\Number\Natural;
+use AGmakonts\STL\Number\Integer;
+/**
+ * Natural test case.
+ */
+class NaturalTest extends PHPUnit_Framework_TestCase
+{
+
+
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp ()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated NaturalTest::setUp()
+
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown ()
+    {
+        // TODO Auto-generated NaturalTest::tearDown()
+
+
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct ()
+    {
+
+    }
+
+    /**
+     * Tests Natural->__construct()
+     * @expectedException AGmakonts\STL\Number\Exception\InvalidValueException
+     */
+    public function test__construct ()
+    {
+        $natural = new Natural(-8);
+    }
+
+    /**
+     * Tests Natural->createFrom()
+     */
+    public function testCreateFrom ()
+    {
+        $negativeInteger = new Integer(-6);
+        $positiveInteger = new Integer(99);
+
+        $negativeTestNatural = Natural::createFrom($negativeInteger);
+        $positiveTestNatural = Natural::createFrom($positiveInteger);
+
+        self::assertEquals(0, $negativeTestNatural->getValue());
+        self::assertEquals(99, $positiveTestNatural->getValue());
+    }
+}
+
