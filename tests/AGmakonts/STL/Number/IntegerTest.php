e0259765bab4b93d4369104c45d4707d2fe4f046
AGmakonts <agmakonts@gmail.com>
2014-10-30 01:00:56 +0100

    phpunit --debug

--- tests/AGmakonts/STL/Number/IntegerTest.php
+++ tests/AGmakonts/STL/Number/IntegerTest.php
@@ -1,30 +1,29 @@
 <?php
-///**
-// * @coversDefaultClass \AGmakonts\STL\String\String
-// */
-//class IntegerTest extends PHPUnit_Framework_TestCase
-//{
-//
-//
-//    /**
-//     * @dataProvider subtractProvider
-//     */
-//    public function testSubtract($int1, $int2, $int3)
-//    {
-//        $starter = \AGmakonts\STL\Number\Integer::get($int1);
-//        $subtract = \AGmakonts\STL\Number\Integer::get($int2);
-//
-//        self::assertEquals($int3, $starter->subtract($subtract)->value());
-//    }
-//
-//    public function subtractProvider()
-//    {
-//        return [
-//            [10 , 2  , 8  ],
-//            [5  , 5  , 0  ],
-//            [10 , 20 , -10],
-//            [5  , 0  , 5  ]
-//        ];
-//    }
-//}
-//
+/**
+ * @coversDefaultClass \AGmakonts\STL\String\String
+ */
+class IntegerTest extends PHPUnit_Framework_TestCase
+{
+
+
+    /**
+     * @dataProvider subtractProvider
+     */
+    public function testSubtract($int1, $int2, $int3)
+    {
+        $starter = \AGmakonts\STL\Number\Integer::get($int1);
+        $subtract = \AGmakonts\STL\Number\Integer::get($int2);
+
+        self::assertEquals($int3, $starter->subtract($subtract)->value());
+    }
+
+    public function subtractProvider()
+    {
+        return [
+            [10 , 2  , 8  ],
+            [5  , 5  , 0  ],
+            [10 , 20 , -10],
+            [5  , 0  , 5  ]
+        ];
+    }
+}
