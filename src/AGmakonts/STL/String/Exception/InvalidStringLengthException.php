bfa8cb7a67fb084db0a7608981efd3098325a66d
Radek Adamiec <radek@procreative.eu>
2015-01-20 12:21:56 +0100

    Added new class RandomString which generates new object String with random value. Also small fix for Integer class

---
+++ src/AGmakonts/STL/String/Exception/InvalidStringLengthException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Radek Adamiec <radek@procreative.eu>
+ * Date: 20.01.15
+ * Time: 11:45
+ */
+
+namespace AGmakonts\STL\String\Exception;
+
+
+class InvalidStringLengthException extends \InvalidArgumentException{
+
+    
+}
